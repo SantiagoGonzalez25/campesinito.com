9db9bf8ba1fe003d79bff897552ea11cbd3fd505
Smilesade <gonzalezsanti997@gmail.com>
2025-10-09 11:08:32 -0500

    Primer commit: proyecto inicial

---
+++ agricolasproductos/src/main/java/com/campesinito/agricolasproductos/AgricolasproductosApplication.java
@@ -0,0 +1,13 @@
+package com.campesinito.agricolasproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgricolasproductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgricolasproductosApplication.class, args);
+	}
+
+}
---
+++ agricolasproductos/src/test/java/com/campesinito/agricolasproductos/AgricolasproductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campesinito.agricolasproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgricolasproductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
